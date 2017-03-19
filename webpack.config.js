aaa7f6776f2f490d25db5c06fe3a350e1c56dae6
Wasa C <gie68i@gmail.com>
2017-03-19 13:28:51 +0700

    uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var HtmlwebpackPlugin = require('html-webpack-plugin');
+var UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
 
 module.exports = {
 	entry: {
@@ -29,6 +30,12 @@ module.exports = {
 		new HtmlwebpackPlugin({
 			title: 'Intro to webpack',
 			template: 'src/index.html'
+		}),
+		new UglifyJsPlugin({
+			beautify: false,
+			mangle: { screw_ie8 : true },
+			compress: { screw_ie8: true, warnings: false },
+			comments: false
 		})
 	]
 }
