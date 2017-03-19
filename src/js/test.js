35e04b3fb81c0f00972a7fb822e9040eba536401
Wasa C <gie68i@gmail.com>
2017-03-19 13:34:23 +0700

    added jquery

--- src/js/test.js
+++ src/js/test.js
@@ -1,4 +1,11 @@
 import testUtil from './utilities/testUtil';
+import $ from 'jquery';
+import jQuery from 'jquery';
+
+// export for others scripts to use
+window.$ = $;
+window.jQuery = jQuery;
 
 var util = new testUtil();
-util.testMethod();
+util.testMethod()
+$('#test-jquery').text('hello world - jquery');
